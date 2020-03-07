d21713ebf73efb0539f8c292cde89730cb23643b
Amir Mourali <amourali7135@gmail.com>
2020-03-07 04:23:10 -0800

    Sleepy time and more progress towards sellerinquiry update for completed

--- app/controllers/sellerinquiries_controller.rb
+++ app/controllers/sellerinquiries_controller.rb
@@ -7,25 +7,30 @@ class SellerinquiriesController < ApplicationController
     @buyer = @inquiry.buyer
     @sellerinquiry.seller = @seller
     @sellerinquiry.inquiry = @inquiry
-    @sellerinquiry.completed = DateTime.current
+    
     if @sellerinquiry.save
       flash[:notice] = "You've successfully joined this inquiry! Follow the instructions and then hit complete"
       redirect_to buyer_inquiry_path(@buyer, @inquiry) 
-      # redirect_to @inquiry
     else
       flash[:notice] = "There was an error, please try again!"
       redirect_to buyer_inquiry_path(@buyer, @inquiry) 
-      # redirect_to @inquiry
     end
-    if @sellerinquiry.completed
+    
+  end
+  
+  def update
+    @seller = current_user.seller if current_user.seller
+    @inquiry = Inquiry.find(params[:sellerinquiry][:inquiry_id])
+    @sellerinquiry = Sellerinquiry.where(seller_id: current_user.seller.id & inquiry_id: @inquiry.id) #It'll go to update action automatically via this line ala Rayhan.  
+    @sellerinquiry.completed = DateTime.current
+    if @sellerinquiry.completed.save
       flash[:notice] = "You've successfully completed this inquiry"
       redirect_to buyer_inquiry_path(@buyer, @inquiry) 
+    else
+      flash[:notice] = "There was an error, please try again!"
+      redirect_to buyer_inquiry_path(@buyer, @inquiry) 
     end
-  end
-
-  def update
-    @sellerinquiry = Sellerinquiry.where(seller_id = current_user.seller.id && inquiry_id = @inquiry.id)
-
+    raise
   end
   
   
