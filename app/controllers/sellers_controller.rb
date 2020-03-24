e3a09989a940dddb68650398e29d031a86735f86
Amir Mourali <amourali7135@gmail.com>
2020-03-24 00:00:29 -0700

    Front page theme added for easy modification

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -2,7 +2,7 @@ class SellersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:submit]
   skip_before_action :authenticate_user!, only: [:index]
   
-  def index
+  def index #do conditional for pagination versus searched.  
     @buyer = current_user.buyer if current_user.buyer
     @inquiries = @buyer.inquiries if current_user.buyer
     @inquiry = Inquiry.where(buyer_id: @buyer.id )
@@ -11,10 +11,10 @@ class SellersController < ApplicationController
       @filter = params["search"]["types"].reject { |type| type == '' }.concat([params["search"]['age']]).concat([params["search"]['min_age']]).concat([params["search"]["country"]]).concat([params["search"]["sex"]]).concat([params["search"]["occupation"]]).concat([params["search"]["city"]]).concat([params["search"]["income"]]).concat([params["search"]["ethnicity"]]).concat([params["search"]["race"]]).concat([params["search"]["religion"]]).concat([params["search"]["sexuality"]]).concat([params["search"]["politics"]]).concat([params["search"]["relationship_status"]]).concat([params["search"]["children"]]).concat([params["search"]["verified"]]).concat([params["search"]["birth_country"]]).concat([params["search"]["smoker"]]).concat([params["search"]["education_level"]]).flatten.reject(&:blank?)
       # @filter = params["search"]#["search"]
       @sellers = Seller.global_search(@filter)
-      @inquiries = current_user.inquiries #is this right to get buyer inquiries?
+      # @inquiries = current_user.inquiries #is this right to get buyer inquiries?
     else
       @sellers = Seller.all
-      @inquiries = current_user.inquiries #is this right to get buyer inquiries?
+      # @inquiries = current_user.inquiries #is this right to get buyer inquiries?
     end
     # raise
   end
@@ -22,6 +22,7 @@ class SellersController < ApplicationController
   def notify
     # raise
     inquiry_ids = params[:inquiry][:ids]
+    seller_ids = params[:seller][:ids]
     # do stuff
       # InquiryMailer.notify_seller(params[:inquiry][:ids])
       # flash notice
