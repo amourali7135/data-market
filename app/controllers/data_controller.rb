7a8e7020220d42b73348fbed019191aeff37e06d
Amir Mourali <amourali7135@gmail.com>
2020-03-27 01:23:13 -0700

    Redundant controllers Heroku fix attempt II

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,54 +1,54 @@
-class DataController < ApplicationController
+# class DataController < ApplicationController
 
-  def new
-    @seller = Seller.find(params[:seller_id])
-    @data = Data.new
-  end
+#   def new
+#     @seller = Seller.find(params[:seller_id])
+#     @data = Data.new
+#   end
 
-  def show
-    @seller = Seller.find(params[:seller_id])
-    @data = Data.find(params[:id])
-  end
+#   def show
+#     @seller = Seller.find(params[:seller_id])
+#     @data = Data.find(params[:id])
+#   end
 
-  def create
-    @data = Data.new(data_params)
-    @seller = Seller.find(params[:seller_id])
-    @data.seller = @seller
-    if @data.save
-      flash[:notice] = "Your buyer profile was successfully created!"
-      redirect_to business_product_path(@seller, @data)
-    else
-      flash[:notice] = "Your buyer profile was successfully created!"
-      render :new
-    end
-  end
+#   def create
+#     @data = Data.new(data_params)
+#     @seller = Seller.find(params[:seller_id])
+#     @data.seller = @seller
+#     if @data.save
+#       flash[:notice] = "Your buyer profile was successfully created!"
+#       redirect_to business_product_path(@seller, @data)
+#     else
+#       flash[:notice] = "Your buyer profile was successfully created!"
+#       render :new
+#     end
+#   end
 
 
-  def update
-    @data = Data.find(params[:id])
-    if @data.update(data_params)
-      flash[:notice] = "Your buyer profile was successfully created!"
-      redirect_to business_product_path(@data.seller, @data) #get from rails routes
-    else
-      flash[:notice] = "Your buyer profile was successfully created!"
-      render 'edit'
-    end
-  end
+#   def update
+#     @data = Data.find(params[:id])
+#     if @data.update(data_params)
+#       flash[:notice] = "Your buyer profile was successfully created!"
+#       redirect_to business_product_path(@data.seller, @data) #get from rails routes
+#     else
+#       flash[:notice] = "Your buyer profile was successfully created!"
+#       render 'edit'
+#     end
+#   end
 
-  def delete
-    @data = Data.find(params[:id])
-    @data.destroy
-    redirect_to products
-  end
+#   def delete
+#     @data = Data.find(params[:id])
+#     @data.destroy
+#     redirect_to products
+#   end
 
-  def edit
-    @data = Data.find(params[:id])
-    @seller = Seller.find(params[:seller_id])
-  end
+#   def edit
+#     @data = Data.find(params[:id])
+#     @seller = Seller.find(params[:seller_id])
+#   end
 
-  private
+#   private
 
-  def data_params
-    params.require(:data).permit(:name, :description, :price_cents, :category, :photo)
-  end
-end
+#   def data_params
+#     params.require(:data).permit(:name, :description, :price_cents, :category, :photo)
+#   end
+# end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,50 +1,50 @@
-class ReviewsController < ApplicationController
-  def new
-    @review = Review.new
-  end
+# class ReviewsController < ApplicationController
+#   def new
+#     @review = Review.new
+#   end
 
-  def create
-    @review = Review.new(review_params)
-    @buyer = Buyer.find(params[:id])
-    @review.user_id = current_user.id
-    if @review.save
-      flash[:notice] = "Your review was successfully created!"
-      redirect_to @review
-    else
-      flash[:notice] = "There was an error, please try again!"
-      render "new"
-    end
-  end
+#   def create
+#     @review = Review.new(review_params)
+#     @buyer = Buyer.find(params[:id])
+#     @review.user_id = current_user.id
+#     if @review.save
+#       flash[:notice] = "Your review was successfully created!"
+#       redirect_to @review
+#     else
+#       flash[:notice] = "There was an error, please try again!"
+#       render "new"
+#     end
+#   end
 
-  def show
-    @review = Review.find(params[:id])
-  end
+#   def show
+#     @review = Review.find(params[:id])
+#   end
 
-  def update
-    @review = Review.find(params[:id])
-    if @review.update(review_params)
-      flash[:notice] = "Your review profile was successfully updated!"
-      redirect_to sellerdashboard_path #Change later.
-    else
-      flash[:error] = "There was an error, please try again!"
-      render 'edit'
-    end
-  end
+#   def update
+#     @review = Review.find(params[:id])
+#     if @review.update(review_params)
+#       flash[:notice] = "Your review profile was successfully updated!"
+#       redirect_to sellerdashboard_path #Change later.
+#     else
+#       flash[:error] = "There was an error, please try again!"
+#       render 'edit'
+#     end
+#   end
 
-  def delete
-    @review = Review.find(params[:id])
-    @review.destroy
-    flash[:notice] = "Your review was successfully deleted!"
-    redirect_to root_path
-  end
+#   def delete
+#     @review = Review.find(params[:id])
+#     @review.destroy
+#     flash[:notice] = "Your review was successfully deleted!"
+#     redirect_to root_path
+#   end
 
-  def edit
-    @review = Review.find(params[:id])
-  end
+#   def edit
+#     @review = Review.find(params[:id])
+#   end
 
-  private
+#   private
 
-  def review_params
-    params.require(:review).permit(:information, :requirements, :usage, :reward, :anonymous, :total, :accept?,  :types, types: [], )
-  end
-end
+#   def review_params
+#     params.require(:review).permit(:information, :requirements, :usage, :reward, :anonymous, :total, :accept?,  :types, types: [], )
+#   end
+# end
